d7387bdf5450b11f5fefdccab76ee419061c58b2
Fynn Forster <fynn.forster@gmx.de>
2026-07-30 18:13:00 +0200

    added part of the glm lib for transformation and scaling math with opengl. will be cleaned up after i know what i need and not need lol

--- scop.hpp
+++ scop.hpp
@@ -5,6 +5,10 @@
 #define GL3_PROTOTYPES 1
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
